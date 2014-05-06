28a5e6493ae8ea7e3806b852bf50dd0ab2e8eed5
Sliim <sliim@mailoo.org>
2014-05-06 21:06:37 +0200

    Bump version 0.1.3

--- metadata.rb
+++ metadata.rb
@@ -19,7 +19,7 @@ maintainer_email 'sliim@mailoo.org'
 license          'Apache 2.0'
 description      'Installs/Configures Damn Vulnerable Web Application'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 
 depends "apache2"
 depends "mysql"
